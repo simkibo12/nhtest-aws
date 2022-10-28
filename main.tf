a2aedd34e18590e687b7c620674c21a6f69482c5
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-10-28 16:37:09 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ terraform {
     bucket = "nh-terraform-bucket"
     key    = "global/s3/terraform.tfstate"
     region = "ap-northeast-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
   }
 }
 
@@ -169,6 +172,19 @@ resource "aws_s3_bucket" "terraform-state" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
 
 
 
