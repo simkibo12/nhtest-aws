d5cc533a83497774777536d3a0e99111f611bd40
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-10-28 15:59:27 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -161,8 +161,6 @@ resource "aws_lb" "nh_alb" {
   #subnets            = [var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id]
   #subnets            =  [for subnet in aws_subnet.new_subnet : subnet.id]
 
-
- enable_deletion_protection = true
   
    subnet_mapping {
     subnet_id            = var.is_portal_subnet == true ? data.aws_subnet.kibo-subnet-01[0].id : aws_subnet.new_subnet[0].id
