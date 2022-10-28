bdfe76577710e2d245c9f000ec1bcc89e5492420
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-10-28 14:48:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_vpc" "selected" {
 
 data "aws_route_table" "portal_route_table" {
   count = var.is_portal_vpc == true ? 0 : 1
-  id = var.subnet_id
+  id = data.aws_subnet.kibo-subnet-01.id
 }
 
 
