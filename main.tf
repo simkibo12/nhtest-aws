4d854af1b55cb1b4cff5910da216e3055181bdfe
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-10-28 15:09:46 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_subnet" "new_subnet" {
   resource "aws_route_table_association" "new_subnet_route_table_association" {
     count = var.is_portal_subnet == false ? 1 : 0
     subnet_id      = aws_subnet.new_subnet[0].id
-    route_table_id = var.is_portal_vpc == false ? aws_route_table.new_route_table[0].id : data.aws_route_table.portal_route_table[0].id
+    route_table_id = var.is_portal_subnet == false ? aws_route_table.new_route_table[0].id : data.aws_route_table.portal_route_table[0].id
   }
 
 
