6afa839df304cc9a26546ee83037e690a320a8bc
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-10-28 16:01:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_route" "new_route" {
   
 resource "aws_subnet" "new_subnet" {
   count = var.is_portal_subnet == false ? 1 : 0
-  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[1].id : data.aws_vpc.selected.id
+  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
   cidr_block              = var.is_portal_subnet == false ? var.new_subnet_cidr_blocks : data.aws_vpc.selected.id
   availability_zone       = "ap-northeast-2a"
   map_public_ip_on_launch = "true"
@@ -59,7 +59,7 @@ resource "aws_subnet" "new_subnet" {
 
 resource "aws_subnet" "lb_subnet" {
   count = var.is_portal_subnet == false ? 1 : 0
-  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[1].id : data.aws_vpc.selected.id
+  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
   cidr_block              = var.is_portal_subnet == false ? var.lb_cidr_blocks : data.aws_vpc.selected.id
   availability_zone       = "ap-northeast-2b"
   map_public_ip_on_launch = "true"
