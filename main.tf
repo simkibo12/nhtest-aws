027fa674a69390fc86e4eb8e1cadf6bb53dd4ee9
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-11-18 11:23:33 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -158,14 +158,13 @@ resource "aws_s3_bucket" "terraform-state" {
     enabled = true
   }
 }
-/*
+
 # object
 resource "aws_s3_bucket_object" "object" {
   bucket = "nh-terraform-bucket"
   key    = "Folder1/"
   source = "/dev/null"
 }
-*/
 
 
 # ALB
