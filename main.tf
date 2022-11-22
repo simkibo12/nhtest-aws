5d92aa3f0ac08b882445e2189d64e0c7d069206f
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-11-22 16:13:39 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,10 +59,10 @@ resource "aws_route" "new_route" {
 resource "aws_subnet" "new_subnet" {
   ##count = var.is_portal_subnet == false ? 1 : 0
   count = var.is_portal_subnet == true ? 1 : 0
-  ##vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
-  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : "vpc-03eede61a3b2599e1"
-  ##cidr_block              = var.is_portal_subnet == false ? var.new_subnet_cidr_blocks : data.aws_vpc.selected.id
-  cidr_block              = var.is_portal_subnet == false ? var.new_subnet_cidr_blocks : "vpc-03eede61a3b2599e1"
+  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
+  ##vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : "vpc-03eede61a3b2599e1"
+  cidr_block              = var.is_portal_subnet == false ? var.new_subnet_cidr_blocks : data.aws_vpc.selected.id
+  ##cidr_block              = var.is_portal_subnet == false ? var.new_subnet_cidr_blocks : "vpc-03eede61a3b2599e1"
   availability_zone       = "ap-northeast-2a"
   map_public_ip_on_launch = "true"
   }
@@ -70,10 +70,10 @@ resource "aws_subnet" "new_subnet" {
 resource "aws_subnet" "lb_subnet" {
   ##count = var.is_portal_subnet == false ? 1 : 0
   count = var.is_portal_subnet == true ? 1 : 0
-  #vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
-  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : "vpc-03eede61a3b2599e1"
-  #cidr_block              = var.is_portal_subnet == false ? var.lb_cidr_blocks : data.aws_vpc.selected.id
-  cidr_block              = var.is_portal_subnet == false ? var.lb_cidr_blocks : "vpc-03eede61a3b2599e1"
+  vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : data.aws_vpc.selected.id
+  #vpc_id                  = var.is_portal_vpc == false ? aws_vpc.new_vpc[0].id : "vpc-03eede61a3b2599e1"
+  cidr_block              = var.is_portal_subnet == false ? var.lb_cidr_blocks : data.aws_vpc.selected.id
+  #cidr_block              = var.is_portal_subnet == false ? var.lb_cidr_blocks : "vpc-03eede61a3b2599e1"
   availability_zone       = "ap-northeast-2b"
   map_public_ip_on_launch = "true"
   }
