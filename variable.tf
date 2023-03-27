62c6f792ae2d5419db3f1234ac9f185b715f8f6d
s2leeee <97569825+s2leeee@users.noreply.github.com>
2023-03-27 15:34:37 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -79,24 +79,8 @@ variable "force_detach" {
         type = string
       }
       
-      /*
-      variable "record_name" {
-           type = list(string)
-      }
-      variable "names" {
-              type = map(object({
-                      {
-                  "a.abc.com"  = {
-                    record_name = "a"
-                      }
-                  "b.def.com"  = {
-                    record_name   = "b"
-                      }
-              }))
-      }
-      */
         variable "names" {
               type = map(object({
-                     host = string
+                     record_name = string
               }))
       }
